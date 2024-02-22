b7c47bdb62bb0ad622549b57a72a54fa148b9674
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-02-22 13:29:09 +0100

    220220241

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-const ApiKey = '42470920-96122d8b93373a33cc6d0556a';
-const searchForm = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const moreLoad = document.querySelector('.load-more');
-const btn = document.querySelector('.btn');
+const ApiKey = "31019872-5203125bb9147bf7b31b034ba"
+const gallery = document.querySelector(".gallery");
+const searchForm= document.querySelector("#search-form");
+const moreLoad = document.querySelector(".load-more");
+const btn = document.querySelector(".btn");
 
 let pageNow = 1;
-let currentSearchName = '';
+let currentSearchName = "";
 let lastPage = 1;
 let autoScroll = false;
 
-
-// код виконує запит до Pixabay для отримання фотографій за заданим ім’ям та сторінкою, 
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 300,
+    scrollZoomFactor: false,});
+	
+    // код виконує запит до Pixabay для отримання фотографій за заданим ім’ям та сторінкою, 
 // обробляє можливі помилки та виводить повідомлення про помилку в консоль або для користувача
 
 const fetchPhotos = async (name, page) => {
@@ -27,11 +29,11 @@ const fetchPhotos = async (name, page) => {
         // image_type - тип малюнку. тут хочемо тільки фото (photo)
         // orientation - орієнтація малюнку (horizontal)
         // safesearch - пошук малюнків SFW (Safe For Work). (true)
-        const response = await axios.get(`https://pixabay.com/api/?${ApiKey}&q={name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
-    const photo = await response.data;
-    checkResults(photo);
-    } catch (error) {
-        console.log(error.message);
+        const respon = await axios(`https://pixabay.com/api/?key=${ApiKey}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+        const photos = await respon.data;
+        checkResults(photos);
+    } catch(error) {
+       
         Notiflix.Notify.failure(error.message);
     }
 };
@@ -39,103 +41,94 @@ const fetchPhotos = async (name, page) => {
 // якщо збігів немає, то виводить помилку
 
 const checkResults = (photos) => {
-    if(photos.hits.length === 0) {
-        Notiflix.Notify.failure('Sorry,there are no images matching your search query. Please try again.');
-    } else {
-if(pageNow === 1) { //якщо одна сторінка 
-    Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`); //виводить повідомлення і кількість знайдених зображень
-    currentSearchName = name; 
-    lastPage = Math.ceil(photos.totalHits / 40);
-    if(lastPage > 1 && autoScroll === false) {  // якщо більше 1 сторінки і скролл вимкнений, то додає клас
-        moreLoad.classList.add('is-visible')
-    };
+     if (photos.hits.length === 0) { 
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");   
+        } else {    
+            if (pageNow === 1) {
+                Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);  //виводить повідомлення і кількість знайдених зображень
+                currentSearchName = name;
+                lastPage = Math.ceil(photos.totalHits / 40);
+                if (lastPage > 1 && autoScroll === false) { // якщо більше 1 сторінки і скролл вимкнений, то додає клас
+                    moreLoad.classList.add("is-visible")
+                };
+            }
+            renderPhotos(photos);
+            lightbox.refresh();
+            if (pageNow > 1) {
+                renderScroll()
+            };
+        }   
 }
-renderPhotos(photos);
-lightbox.refresh();
-if(pageNow > 1) {
-    renderScroll()
-};
-    };
-};
 
-let lightbox = new SimpleLightbox('.gallery a', {
-    captionDelay: 300,
-    scrollZoomFactor: false,
-});
+// якщо знаходить фото, то додає нові пункти, які відповідають до фото
 
-// подія, яка при натисканні оновлює поточну сторінку, очищує вміст галереї 
-// та викликає функцію для отримання фотографій
-searchForm.addEventListener('submit',(event) => {
- event.preventDefault(); //щоб сторінка сама не перезавантажувалась
-pageNow = 1;
-gallery.innerHTML = '';
-fetchPhotos(searchForm.searchQuery.value.pageNow);
-});
-
-const renderPhotos = photo => {
+const renderPhotos = (photos) => {
     const markup = photos.hits.map((photo) => {
-        return 
-        `<div class="photo-card">
-        <a href="${photo.largeImageURL}"> 
-            <img src="${photo.webformatURL}" alt="${photo.tags}" data-source="${photo.largeImageURL}" loading="lazy" />
-            <div class="info">
-            <p class="info-item"><span><b>Likes</b></span><span>${photo.likes}</span></p>
-            <p class="info-item"><span><b>Views</b></span><span>${photo.views}</span></p>
-            <p class="info-item"><span><b>Comments</b></span><span>${photo.comments}</span></p>
-            <p class="info-item"><span><b>Downloads</b></span><span>${photo.downloads}</span></p>
-            </div>
-        </a> 
-    </div>`
+        return `<div class="photo-card">
+                    <a href="${photo.largeImageURL}"> 
+                        <img src="${photo.webformatURL}" alt="${photo.tags}" data-source="${photo.largeImageURL}" loading="lazy" />
+                        <div class="info">
+                        <p class="info-item"><span><b>Likes</b></span><span>${photo.likes}</span></p>
+                        <p class="info-item"><span><b>Views</b></span><span>${photo.views}</span></p>
+                        <p class="info-item"><span><b>Comments</b></span><span>${photo.comments}</span></p>
+                        <p class="info-item"><span><b>Downloads</b></span><span>${photo.downloads}</span></p>
+                        </div>
+                    </a> 
+                </div>`
     })
-    .join('')
-gallery.innerHTML = gallery.innerHTML + markup;
-};
-
-// ??????????????????????? рішить самій!!!!!!!!!
+    .join("")
+    gallery.innerHTML = gallery.innerHTML + markup;
+}
 
 const renderScroll = () => {
-    const infoPhoto = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
-    const {height: cardHeight} = infoPhoto;
+    const photoInfo = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } = photoInfo;
     window.scrollBy({
         top: cardHeight * 2,
-        behavior: 'smooth',
+        behavior: "smooth",
     })
 }
-
+ 
 const infinityScroll = _throttle(() => {
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-        if(pageNow < lastPage) {
+        if (pageNow < lastPage) {
             pageNow++;
-            fetchPhotos(currentSearchName, pageNow);
-            if(pageNow === lastPage) {
-                Notiflix.Notify.info('Sorry, but you have reached the end of the search results.');
-                moreLoad.classList.remove('is-visible');
-            }
-        }
+            fetchPhotos(currentSearchName, pageNow);     
+            if (pageNow === lastPage) {
+                Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); 
+                moreLoad.classList.remove("is-visible");
+            }  
+        } 
     }
-}, 200)
+}, 250)
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    pageNow = 1;
+    gallery.innerHTML = "";
+    fetchPhotos(searchForm.searchQuery.value, pageNow);
+})
 
 btn.addEventListener('click', () => {
-    if(autoScroll === false) {
+    if (autoScroll === false) {
         autoScroll = true;
         window.addEventListener('scroll', infinityScroll);
-        Notiflix.Notify.info('Loading method has been changed to auto.');
+        Notiflix.Notify.info("Loading method has been changed to auto.");
     } else {
         autoScroll = false;
-        window.addEventListener('scroll', infinityScroll);
-        Notiflix.Notify.info('Loading method has been changed to manual.');
-    }
-    btn.classList.toggle('is-active');
-    if(pageNow != lastPage) {
-        moreLoad.classList.toggle('is-visible');
-    };
-});
+        window.removeEventListener('scroll', infinityScroll);
+        Notiflix.Notify.info("Loading method has been changed to manual.");    
+    }  
+    btn.classList.toggle("is-active");
+    if (pageNow !== lastPage){moreLoad.classList.toggle("is-visible");} 
+})
 
 moreLoad.addEventListener('click', () => {
     pageNow++;
-    fetchPhotos(currentSearchName, pageNow);
+    fetchPhotos(currentSearchName, pageNow);     
     if(pageNow === lastPage) {
-        Notiflix.Notify.info('Sorry, but you have reached the end of the search results.');
-        moreLoad.classList.remove('.is-visible');
-    };
+        Notiflix.Notify.info("Sorry, but you have reached the end of the search results."); 
+        moreLoad.classList.remove("is-visible");
+    }  
 })
+
