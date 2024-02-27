a9e4150f3493b612dc09cc15f0b89c24f4544449
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-02-27 09:15:25 +0100

    27022024

--- src/index.js
+++ src/index.js
@@ -6,20 +6,20 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import InfiniteScroll from 'infinite-scroll';
 
 
-const APIKEY = '31019872-5203125bb9147bf7b31b034ba'
+const APIKEY = '31019872-5203125bb9147bf7b31b034ba';
 const gallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('#search-form');
 
 let pageNow = 1;
 let currentSearchName = '';
 let lastPage = 1;
-let autoScroll = false;
+
 
 let lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 300,
     scrollZoomFactor: false,
-});
-	
+  });
+
     // код виконує запит до Pixabay для отримання фотографій за заданим ім’ям та сторінкою, 
 // обробляє можливі помилки та виводить повідомлення про помилку в консоль або для користувача
 
@@ -30,13 +30,14 @@ const fetchPhotos = async (name, page) => {
         // image_type - тип малюнку. тут хочемо тільки фото (photo)
         // orientation - орієнтація малюнку (horizontal)
         // safesearch - пошук малюнків SFW (Safe For Work). (true)
-        const response = await axios.get(`https://pixabay.com/api/?key=${APIKEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
-        const photos = await response.data;
-        checkResults(photos);
-    } catch(error) {
-        Notiflix.Notify.failure(error.message);
-    }
+       const response = await axios.get(`https://pixabay.com/api/?key=${APIKEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+    const photos =  response.data;
+    checkResults(photos);
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
+  }
 };
+
 
 // якщо збігів немає, то виводить помилку
 
@@ -54,29 +55,32 @@ const checkResults = (photos) => {
             lightbox.refresh();
             if (pageNow > 1) {
                 renderScroll()
-            };
+            }
         }   
-}
+};
 
 // якщо знаходить фото, то додає нові пункти, які відповідають до фото
 
 const renderPhotos = (photos) => {
-    const markup = photos.hits.map((photo) => {
-        return `<div class="photo-card">
-                    <a href="${photo.largeImageURL}"> 
-                        <img src="${photo.webformatURL}" alt="${photo.tags}" data-source="${photo.largeImageURL}" loading="lazy" width="250" height="300"/>
-                        <div class="info">
-                        <p class="info-item"><span><b>Likes</b></span><span>${photo.likes}</span></p>
-                        <p class="info-item"><span><b>Views</b></span><span>${photo.views}</span></p>
-                        <p class="info-item"><span><b>Comments</b></span><span>${photo.comments}</span></p>
-                        <p class="info-item"><span><b>Downloads</b></span><span>${photo.downloads}</span></p>
-                        </div>
-                    </a> 
-                </div>`
-    })
-    .join("")
-    gallery.innerHTML = gallery.innerHTML + markup;
-}
+    const markup = photos.hits
+      .map((photo) => `
+        <div class="photo-card">
+          <a href="${photo.largeImageURL}">
+            <img src="${photo.webformatURL}" alt="${photo.tags}" 
+            data-source="${photo.largeImageURL}" loading="lazy" width="250" height="300"/>
+            <div class="info">
+              <p class="info-item"><span><b>Likes</b></span><span>${photo.likes}</span></p>
+              <p class="info-item"><span><b>Views</b></span><span>${photo.views}</span></p>
+              <p class="info-item"><span><b>Comments</b></span><span>${photo.comments}</span></p>
+              <p class="info-item"><span><b>Downloads</b></span><span>${photo.downloads}</span></p>
+            </div>
+          </a>
+        </div>
+      `)
+      .join('');
+  
+    gallery.innerHTML += markup;
+  };
 
 const renderScroll = () => {
     const photoInfo = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
@@ -85,7 +89,10 @@ const renderScroll = () => {
         top: cardHeight * 2,
         behavior: "smooth",
     });
+if(pageNow === lastPage) {
+    Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
 }
+};
 
 // якщо натиснути на пустий пошук, то видасть помилку
 
@@ -105,14 +112,15 @@ searchForm.addEventListener('submit', (event) => {
 });
 
 const infinityScroll = _throttle(() => {
-    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
-      if (pageNow < lastPage) {
-        pageNow++;
-        fetchPhotos(currentSearchName, pageNow);
-        if (pageNow === lastPage) {
-          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-        }
+  if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight) {
+    if (pageNow < lastPage) {
+      pageNow++;
+      fetchPhotos(currentSearchName, pageNow);
+      if (pageNow === lastPage) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       }
     }
-  }, 250);
+  }
+
+}, 250);
   window.addEventListener('scroll', infinityScroll);
